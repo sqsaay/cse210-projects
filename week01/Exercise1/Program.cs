b1cbb506c3dda76bb0ae4adcc4696c7e6e915ed2
sqsaay <ayeleth.quevedo@gmail.com>
2025-11-08 17:52:16 -0600

    Exercise 2 done - SS

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your first name? ");
+        string name = Console.ReadLine();
+        Console.WriteLine("What is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {name} {lastName}.");
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int gradePercentage = int.Parse(input);
+        string letterGrade = "";
+
+
+        if (gradePercentage >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        int lastDigit = gradePercentage % 10;
+        string signGrade = "";
+        if (lastDigit >= 7 && letterGrade != "A" && letterGrade != "F")
+        {
+            signGrade = "+";
+        }
+        else if (lastDigit <= 3 && letterGrade != "F")
+        {
+            signGrade = "-";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letterGrade}{signGrade}");
+
     }
+
 }
