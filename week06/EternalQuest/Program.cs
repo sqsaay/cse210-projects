b49130d1dc6423eb097ca4028168a71adcd61b47
sqsaay <ayeleth.quevedo@gmail.com>
2025-12-11 17:55:03 -0600

    v01 - Running Tracking

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,4 +1,3 @@
-
 namespace EternalQuest;
 
 class Program
---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,3 @@
+03 Nov 2022 Running (30 min) - Distance 4.8 km, Speed 9.6 kph, Pace 6.25 min per km
+04 Nov 2022 Cycling (40 min) - Distance 10.0 km, Speed 15.0 kph, Pace 4.00 min per km
+05 Nov 2022 Swimming (25 min) - Distance 1.0 km, Speed 2.4 kph, Pace 25.00 min per km
